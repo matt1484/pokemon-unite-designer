58bfa825b7eca21909d1fb274c038b1d26abde6d
matt1484 <abrego.matt@gmail.com>
2026-01-02 20:33:17 -0500

    Update publicPath for production environment

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? 'https://cdn.statically.io/gh/matt1484/pokemon-unite-designer/master/docs' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? 'https://raw.githubusercontent.com/matt1484/pokemon-unite-designer/refs/heads/master/docs' : '/',
   devServer: {
     host: '127.0.0.1',
   }
